52b6ad60d594e0d361bfef8641857cd85930f444
pardeep <pardeepkambojj@gmail.com>
2022-06-09 21:05:15 +0530

    first commit

--- testcases/test_searchresult.py
+++ testcases/test_searchresult.py
@@ -1,16 +1,16 @@
 import pytest
 from pages.test_launchpages import test_launchpage
-# from ddt import ddt, data, file_data, idata, unpack
+from ddt import ddt, data, file_data, idata, unpack
 @pytest.mark.usefixtures("setup")
-# @ddt
-class Test_results():
+@ddt
+class TestFlightResults():
 
-    # @data(("New Delhi" ,"LON" ,"22/08/2022"))
-    # @unpack
-    def test_checking(self):
+    @data(("New Delhi" ,"LON" ,"22/08/2022"))
+    @unpack
+    def test_checking(self,depart,goingto,date):
         lp = test_launchpage(self.driver)
-        lp.flight_search_result("New Delhi" ,"LON" ,"22/08/2022")
-        # lp.flight_search_result(depart,goingto,date)
+        # lp.flight_search_result("New Delhi" ,"LON" ,"22/08/2022")
+        lp.flight_search_result(depart,goingto,date)
 
 
 
