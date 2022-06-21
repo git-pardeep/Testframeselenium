fc221ae7d2d8e049fa08f580b869693f3d746174
git-pardeep <104703897+git-pardeep@users.noreply.github.com>
2022-06-21 14:37:11 +0530

    Update test_searchresult.py

--- testcases/test_searchresult.py
+++ testcases/test_searchresult.py
@@ -4,15 +4,13 @@ from ddt import ddt, data, file_data, idata, unpack
 @pytest.mark.usefixtures("setup")
 # @ddt
 class TestFlightResults():
-    depart ="New Delhi"
-    goingto= "LON"
-    date="22/08/2022"
+    
 #     @data(("New Delhi" ,"LON" ,"22/08/2022"))
 #     @unpack
     def test_checking(self,depart,goingto,date):
         lp = test_launchpage(self.driver)
-        # lp.flight_search_result("New Delhi" ,"LON" ,"22/08/2022")
-        lp.flight_search_result(self.depart,self.goingto,self.date)
+        lp.flight_search_result("New Delhi" ,"LON" ,"22/08/2022")
+#         lp.flight_search_result(depart,goingto,date)
 
 
 
